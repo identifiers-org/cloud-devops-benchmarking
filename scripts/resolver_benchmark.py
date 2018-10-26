6b9be25677d49551c602e02d15aaee55d39306a2
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-26 15:34:56 +0100

    resolver benchmark placeholder

---
+++ scripts/resolver_benchmark.py
@@ -0,0 +1,10 @@
+# 
+# Project   : cloud-devops-benchmarking
+# Timestamp : 26-10-2018 15:34
+# Author    : Manuel Bernal Llinares <mbdebian@gmail.com>
+# ---
+# 
+
+"""
+Document your module here
+"""
