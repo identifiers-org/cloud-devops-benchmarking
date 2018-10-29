771bc5a43c75ba9ab65411534ce4a85a4a7e53c0
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-29 09:47:32 +0000

    get logging

--- scripts/resolver_benchmark.py
+++ scripts/resolver_benchmark.py
@@ -9,6 +9,8 @@
 The goal of this script is to create a benchmark report on identifiers.org Resolution API Service
 """
 
+import logging
+
 # General Algorithm
 # TODO Get resolution dataset
 # TODO Iterate over namespaces requesting the resolver to solve a sample compact identifier within each namespace
