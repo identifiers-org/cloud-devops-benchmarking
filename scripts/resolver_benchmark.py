0907c7d9557886bf2f520d0d60df06599966b40c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-30 11:01:09 +0000

    bug fix using pandas to compute the mode of the response time column

--- scripts/resolver_benchmark.py
+++ scripts/resolver_benchmark.py
@@ -272,7 +272,7 @@ def present_response_times_stats(response_times_dataset):
     logger.info("Successful tests: #{} ({:.2f}%)".format(response_times_dataset.get_number_success_entries(), success_rate))
     logger.info("ERROR Rate: {:.2f}%".format(error_rate))
     logger.info("Mean response time: {:.2f}".format(response_times_dataset.get_response_time_arithmetic_mean()))
-    logger.info("Mode response time: {}".format(response_times_dataset.get_response_time_mode()[1]))
+    logger.info("Mode response time: {}".format(response_times_dataset.get_response_time_mode()[0]))
     logger.info("Median response time: {}".format(response_times_dataset.get_response_time_median()))
     logger.info("Standard Deviation response time: {:.2f}".format(response_times_dataset.get_response_time_standard_deviation()))
     logger.info("----------------------------")
