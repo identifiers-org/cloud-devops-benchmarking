b0e5fa21b02934a305eb73db86c5c37c1151cf18
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-30 11:53:04 +0000

    terraform based provisioning, placeholder, main

--- scripts/resolver_benchmark.py
+++ scripts/resolver_benchmark.py
@@ -283,7 +283,7 @@ def main():
     # Get resolution dataset
     compact_identifiers = get_compact_identifiers_dataset()
     # Measure response time
-    response_times, response_times_dataset = get_response_times_for_compact_identifiers(grow_dataset(compact_identifiers[:10], 10))
+    response_times, response_times_dataset = get_response_times_for_compact_identifiers(grow_dataset(compact_identifiers, 1000))
     print("Response Times description:\n{}".format(stats.describe(response_times)))
     # Print Response times statistics
     present_response_times_stats(response_times_dataset)
