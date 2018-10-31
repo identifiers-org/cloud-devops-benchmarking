e89c17726ac5e0f3525bea19a98383731daf48c0
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-31 11:50:26 +0000

    prepare the machine

---
+++ provisioning/init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This is the initialization script for the VM itself, to prepare all the environment for running the benchmarks
+# Update and upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
+# Prepare software
+sudo apt-get install -y build-essential python3-pip python3-virtualenv virtualenv git
