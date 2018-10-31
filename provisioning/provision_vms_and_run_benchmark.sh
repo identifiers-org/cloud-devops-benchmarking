14cd194bb1082c747d2f9e8154a160e68ec32f7c
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-31 11:59:46 +0000

    script name change

--- provisioning/provision_vms_and_run_benchmark.sh
+++ provisioning/provision_vms_and_run_benchmark.sh
@@ -4,6 +4,7 @@ folder_reports='reports'
 pending_zones=( us-central1 australia-southeast1 asia-east1 asia-northeast1 asia-south1 )
 regions=( europe-west4 )
 vm_name_prefix='resolver-benchmarkvm'
+vm_init_script="${folder_script_home}"/vminit.sh
 
 # Logging subsystem
 source "${folder_script_home}"/tinylogger.bash
