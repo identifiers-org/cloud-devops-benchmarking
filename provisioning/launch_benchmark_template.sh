9f9d641b85dd9061a4f2f68dcae11c7ea2ed6387
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-31 11:53:55 +0000

    template for launching the benchmark

--- provisioning/launch_benchmark_template.sh
+++ provisioning/launch_benchmark_template.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-# Update and upgrade
-sudo apt-get update
-sudo apt-get -y upgrade
-# Prepare software
-sudo apt-get install -y build-essential python3-pip python3-virtualenv virtualenv git
-git clone https://github.com/identifiers-org/cloud-devops-benchmarking.git app
-cd app
-alias pip='pip3'
-make install
+# This is a template script to launch the benchmark
+# Author: Manuel Bernal Llinares <mbdebian@gmail.com>
 
 # Templating
 RESOLVER_HOST=identifiers.org
@@ -20,5 +13,4 @@ BENCHMARK_ORIGIN_NAME=`hostname`
 source python_install/bin/activate
 
 # Run the benchmark (all the environment variables are set by the calling client)
-export RESOLVER_HOST=identifiers.org; export RESOLVER_PROTOCOL=https; export TARGET_RESOLVER_SERVICE_NAME=ebi; export REQUEST_MODE=noapi; export BENCHMARK_ORIGIN_NAME=`hostname`; python scripts/resolver_benchmark.py
-#python scripts/resolver_benchmark.py
+python scripts/resolver_benchmark.py
