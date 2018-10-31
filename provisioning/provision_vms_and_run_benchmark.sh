3d2d7f7f48b43287369dd31f06f2c164c92003d5
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-31 15:29:53 +0000

    all enabled

--- provisioning/provision_vms_and_run_benchmark.sh
+++ provisioning/provision_vms_and_run_benchmark.sh
@@ -33,7 +33,7 @@ for region in "${regions[@]}"; do
     echo -e "\tZone  : ${zone}"
     echo -e "\tVM    : ${vm_name}"
     echo -e "\tLaunching VM..."
-    #gcloud -q compute --project=${google_cloud_project} instances create ${vm_name} --zone=${zone} --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=${google_cloud_service_account} --scopes=https://www.googleapis.com/auth/cloud-platform --image=debian-9-stretch-v20181011 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=${vm_name} --labels=app=resolver-benchmark,region=${region}
+    gcloud -q compute --project=${google_cloud_project} instances create ${vm_name} --zone=${zone} --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=${google_cloud_service_account} --scopes=https://www.googleapis.com/auth/cloud-platform --image=debian-9-stretch-v20181011 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=${vm_name} --labels=app=resolver-benchmark,region=${region}
     vm_external_ip=`gcloud compute instances list --filter=${vm_name} --format=yaml | grep natIP | cut -f2 -d':' | awk '{$1=$1;print}'`
     echo -e "\tVM External IP: ${vm_external_ip}"
     created_vms_and_zones["${vm_name}"]="${zone}"
@@ -41,12 +41,11 @@ for region in "${regions[@]}"; do
     created_vms+="${vm_name}"
     ssh-keygen -R ${vm_external_ip}
     echo -e "\tInit VM... (${vm_script_init})"
-    #scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" "${vm_script_init}" ${vm_external_ip}:~/.
-    #ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" ${vm_external_ip} "bash `basename ${vm_script_init}`"
+    scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" "${vm_script_init}" ${vm_external_ip}:~/.
+    ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" ${vm_external_ip} "bash `basename ${vm_script_init}`"
     echo -e "\tInstall benchmarking application..."
-    echo "TODO"
-    #scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" "${vm_script_app_install}" ${vm_external_ip}:~/.
-    #ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" ${vm_external_ip} "bash `basename ${vm_script_app_install}`"
+    scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" "${vm_script_app_install}" ${vm_external_ip}:~/.
+    ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null" ${vm_external_ip} "bash `basename ${vm_script_app_install}`"
     file_ebi_benchmark="${folder_tmp}"/"${vm_name}-ebi.sh"
     file_cloud_benchmark="${folder_tmp}"/"${vm_name}-cloud.sh"
     echo -e "\tConfiguring benchmark for ebi '${file_ebi_benchmark}'"
