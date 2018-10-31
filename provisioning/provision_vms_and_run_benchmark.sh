52df9ae26f76a075e6799593c68da30a2306e909
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-31 11:00:21 +0000

    tiny logger from github

--- provisioning/provision_vms_and_run_benchmark.sh
+++ provisioning/provision_vms_and_run_benchmark.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-folder_script_home=$(basename $0)
+folder_script_home=$(dirname "$0")
 folder_reports='reports'
 regions=( europe-west4 us-central1 australia-southeast1 asia-east1 asia-northeast1 asia-south1 )
+
+# Logging subsystem
+source "${folder_script_home}"/tinylogger.bash
+
+# Set the logging level
+LOGGER_LVL=debug
+
+for region in ${regions}; do
+    echo "---> Region: ${region}"
+done
