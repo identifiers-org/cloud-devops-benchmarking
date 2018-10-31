0068f88a7d121c0a4301a8ede5b80bbcbe1727a4
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-31 12:44:28 +0000

    creating instances of benchmark files

--- provisioning/provision_vms_and_run_benchmark.sh
+++ provisioning/provision_vms_and_run_benchmark.sh
@@ -24,10 +24,23 @@ for region in "${regions[@]}"; do
     echo -e "\tZone  : ${zone}"
     echo -e "\tVM    : ${vm_name}"
     echo -e "\tLaunching VM..."
-    gcloud -q compute --project=${google_cloud_project} instances create ${vm_name} --zone=${zone} --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=${google_cloud_service_account} --scopes=https://www.googleapis.com/auth/cloud-platform --image=debian-9-stretch-v20181011 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=${vm_name} --labels=app=resolver-benchmark,region=${region}
-    vm_external_ip=`gcloud compute instances list --filter=${vm_name} --format=yaml | grep natIP | cut -f2 -d':' | awk '{$1=$1;print}'`
+    #gcloud -q compute --project=${google_cloud_project} instances create ${vm_name} --zone=${zone} --machine-type=n1-standard-1 --subnet=default --network-tier=PREMIUM --maintenance-policy=MIGRATE --service-account=${google_cloud_service_account} --scopes=https://www.googleapis.com/auth/cloud-platform --image=debian-9-stretch-v20181011 --image-project=debian-cloud --boot-disk-size=10GB --boot-disk-type=pd-standard --boot-disk-device-name=${vm_name} --labels=app=resolver-benchmark,region=${region}
+    #vm_external_ip=`gcloud compute instances list --filter=${vm_name} --format=yaml | grep natIP | cut -f2 -d':' | awk '{$1=$1;print}'`
     echo -e "\tVM External IP: ${vm_external_ip}"
-    ssh-keygen -R ${vm_external_ip}
-    created_vms_and_zones=( ["${vm_name}"]="${zone}" "${created_vms_and_zones[@]}" )
-    created_vms_and_ips=( ["${vm_name}"]="${vm_external_ip}" "${created_vms_and_ips[@]}" )
+    #created_vms_and_zones=( ["${vm_name}"]="${zone}" "${created_vms_and_zones[@]}" )
+    #created_vms_and_ips=( ["${vm_name}"]="${vm_external_ip}" "${created_vms_and_ips[@]}" )
+    echo -e "\tInit VM..."
+    echo "TODO"
+    echo -e "\tInstall benchmarking application..."
+    echo "TODO"
+    file_ebi_benchmark="${folder_tmp}"/"${vm_name}-ebi.sh"
+    file_cloud_benchmark="${folder_tmp}"/"${vm_name}-cloud.sh"
+    echo -e "\tConfiguring benchmark for ebi '${file_ebi_benchmark}'"
+    echo "TODO"
+    echo -e "\tConfiguring benchmark for cloud deployment, '${file_cloud_benchmark}'"
+    echo "TODO"
+    echo -e "\tLaunching benchmarks"
+    echo "TODO"
 done
+
+echo "---> Starting"
