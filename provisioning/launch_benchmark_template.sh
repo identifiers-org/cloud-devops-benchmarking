00c6d4145602a749cdfa0b340f348cb04828ea01
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-31 11:29:57 +0000

    typo in my startup script

--- provisioning/launch_benchmark_template.sh
+++ provisioning/launch_benchmark_template.sh
@@ -3,7 +3,7 @@
 sudo apt-get update
 sudo apt-get -y upgrade
 # Prepare software
-sudo apg-get install -y build-essential python3-pip python3-virtualenv virtualenv git
+sudo apt-get install -y build-essential python3-pip python3-virtualenv virtualenv git
 git clone https://github.com/identifiers-org/cloud-devops-benchmarking.git app
 cd app
 alias pip='pip3'
