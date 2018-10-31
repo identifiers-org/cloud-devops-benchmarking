512aea61e2f24c3c8442b47da9ec21b26e95e3e0
Manuel Bernal Llinares <mbdebian@gmail.com>
2018-10-31 11:08:38 +0000

    print out some information of what's going on

--- provisioning/provision_vms_and_run_benchmark.sh
+++ provisioning/provision_vms_and_run_benchmark.sh
@@ -2,6 +2,7 @@
 folder_script_home=$(dirname "$0")
 folder_reports='reports'
 regions=( europe-west4 us-central1 australia-southeast1 asia-east1 asia-northeast1 asia-south1 )
+vm_name_prefix='resolver-benchmarkvm'
 
 # Logging subsystem
 source "${folder_script_home}"/tinylogger.bash
@@ -9,6 +10,10 @@ source "${folder_script_home}"/tinylogger.bash
 # Set the logging level
 LOGGER_LVL=debug
 
-for region in ${regions}; do
+for region in "${regions[@]}"; do
+    zone="${region}-a"
+    vm_name="${vm_name_prefix}-${region}"
     echo "---> Region: ${region}"
+    echo -e "\tZone  : ${zone}"
+    echo -e "\tVM    : ${vm_name}"
 done
